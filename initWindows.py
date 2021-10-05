8eaae8f4e378ecb182fa35c5074fb019b61f3649
dongsub-joung <joungdongsub1103@gmail.com>
2021-10-05 23:36:08 +0900

    pseudo code

--- initWindows.py
+++ initWindows.py
@@ -1,35 +1,52 @@
-improt os
+import os
 frome datetime import date
 
-const PATH= "C:\Users\test\Documents\Sound recordings"
-const DEFAULT= "Recording"
-const FIRST= "Recording (autosaved)"
+# Define value
+PATH= "C:\Users\test\Documents\Sound recordings"
+DEFAULT= "Recording"
+FORM= "M4a"
+RECORING_FILES= "Recording (autosaved)"
 DATE= today.strftime("%d%m%Y")
 
 file_name= " "
 data_created= " "
 date_modified= " "
 
-# names= os.getFilseNames
-# data_created= os.get
-# date_modified= os.get
+# Main
+file_name= os.getFilseNames
+data_created= os.path.getmtime(file_name)
+date_modified= os.getctime(file_name)
 
 makeFolders(data_created, date_modified)
 file_name= fileNameing(names)
 
-def moveEachPath(){
 
-}
+# Functions
+def saveFileInfoInTXT(file_names, data_node):
+    with open('data.txt', 'w') as f:
+    data = 'some data to be written to the file'
+    f.write(data)
+    with open(f'{data_node}', 'W') as f:
+        file_nodes_info= 'DefaultSentence'
+        file_nodes_info= file_node= os.stat(file_names) + ''
+        f.write(file_nodes_info)
 
-def fileNameing(...names, ...created, ...modified){
-    TIME= created + modified
-    for file_name in names:  
-        if(file_name == DEFAULT){
-            pass;
-        } elif(file_name == FIRS) {
+def moveDateInPath(file_count):
+    PRESENT= RECORING_FILES+FORM
+
+    if PRESENT == file_name:
+        pass
+
+def getFileName():
+    return os.getFilseNames
+    
+def fileNameing(names,created, modified):
+    TIME= created+"_"+modified
+    for file_name in names:
+        if(file_name == DEFAULT):
+            pass
+        elif(file_name == FIRS):
             return DATE+"_1"
-        } else {
+        else:
             numbers= file_name[-2]
             return DATE+numbers
-        }
-}
