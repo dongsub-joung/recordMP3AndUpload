340e215fec93c31a229d30431fb6d90f4961cc2b
dongsub-joung <joungdongsub1103@gmail.com>
2021-10-08 10:38:29 +0900

    IO try-cach

--- README.md
+++ README.md
@@ -1,9 +1,63 @@
-# Record Sound Files And Upload automatly
+# Record Sound Files And Upload automatly  
 
-1. Scann the folder in path
-2. if new files are, perblishing online.
-3. when back around the host-service use, need it network.
-3.2 Before uploading, translate from "audio file form" to "picture or video form"
+## Progress Arrow  
+1. Scann the relative directory(For to get files info).    
+2. Make the folder on date.  
+3. converte the files name to "generated-moified"  
+4. Upload the files, then midifiy the folder's title at time.  
+5. voice recorder make the new dir for saving.  
+> if new files are, perblishing on and repeating.
+
+* when if you back around the host-service, you need it(ufile.io)   
+* Before uploading, do auto-transfomation from "audio file form" to "picture form   
 
 ## ENV
 - Python 3
+- https://ufile.io  
+
+## Files Usage  
+
+- Node File Valuable infomation(Once get "stat_result" when init)  
+    by OS __init__.pyi  
+
+```py
+st_mode: int  # protection bits,
+    st_ino: int  # inode number,
+    st_dev: int  # device,
+    st_nlink: int  # number of hard links,
+    st_uid: int  # user id of owner,
+    st_gid: int  # group id of owner,
+    st_size: int  # size of file, in bytes,
+    st_atime: float  # time of most recent access,
+    st_mtime: float  # time of most recent content modification,
+    st_ctime: float  # platform dependent (time of most recent metadata change on Unix, or the time of creation on Windows)
+    st_atime_ns: int  # time of most recent access, in nanoseconds
+    st_mtime_ns: int  # time of most recent content modification in nanoseconds
+    st_ctime_ns: int  # platform dependent (time of most recent metadata change on Unix, or the time of creation on Windows) in nanoseconds
+    if sys.version_info >= (3, 8) and sys.platform == "win32":
+        st_reparse_tag: int
+    if sys.platform == "win32":
+        st_file_attributes: int
+    def __getitem__(self, i: int) -> int: ...
+    # not documented
+    def __init__(self, tuple: Tuple[int, ...]) -> None: ...
+    # On some Unix systems (such as Linux), the following attributes may also
+    # be available:
+    st_blocks: int  # number of blocks allocated for file
+    st_blksize: int  # filesystem blocksize
+    st_rdev: int  # type of device if an inode device
+    st_flags: int  # user defined flags for file
+
+    # On other Unix systems (such as FreeBSD), the following attributes may be
+    # available (but may be only filled out if root tries to use them):
+    st_gen: int  # file generation number
+    st_birthtime: int  # time of file creation
+
+    # On Mac OS systems, the following attributes may also be available:
+    st_rsize: int
+    st_creator: int
+    st_type: int
+```  
+
+## Reference  
+- https://help.ufile.io/en/article/upload-files-1p925sk/  
--- initWindows.py
+++ initWindows.py
@@ -1,52 +1,94 @@
+# Even when i made about "https://github.com/dongsub-joung/Sociology_bookList",
+# always always i'm alone, had have not a group in or main.
+# Korean is always be korean, so "korea" means the same "the lowest trust society and community commpletly".
+
+# import datetime
 import os
-frome datetime import date
+from datetime import date
 
 # Define value
-PATH= "C:\Users\test\Documents\Sound recordings"
 DEFAULT= "Recording"
-FORM= "M4a"
-RECORING_FILES= "Recording (autosaved)"
-DATE= today.strftime("%d%m%Y")
+FORM= ".M4a"
 
-file_name= " "
-data_created= " "
-date_modified= " "
-
-# Main
-file_name= os.getFilseNames
-data_created= os.path.getmtime(file_name)
-date_modified= os.getctime(file_name)
-
-makeFolders(data_created, date_modified)
-file_name= fileNameing(names)
-
+# Simple functions
+def getIndex(index: int, array: list):
+    for _ in array:
+        index+= 1
+    return index
 
 # Functions
-def saveFileInfoInTXT(file_names, data_node):
-    with open('data.txt', 'w') as f:
-    data = 'some data to be written to the file'
-    f.write(data)
-    with open(f'{data_node}', 'W') as f:
-        file_nodes_info= 'DefaultSentence'
-        file_nodes_info= file_node= os.stat(file_names) + ''
-        f.write(file_nodes_info)
-
-def moveDateInPath(file_count):
-    PRESENT= RECORING_FILES+FORM
-
-    if PRESENT == file_name:
-        pass
-
+# Getter
+def getFileNames():
+    try:
+        return os.listdir('my_dir/')
+    except IOError as e:
+        print(e+"file names")
+        
 def getFileName():
-    return os.getFilseNames
-    
-def fileNameing(names,created, modified):
-    TIME= created+"_"+modified
-    for file_name in names:
-        if(file_name == DEFAULT):
-            pass
-        elif(file_name == FIRS):
-            return DATE+"_1"
-        else:
-            numbers= file_name[-2]
-            return DATE+numbers
+    try:
+        return os.getFilseNames
+    except IOError as e:
+        print(e+"file name")
+
+def getCreatedTime(file_name: str):
+    try:
+        return os.path.getmtime(file_name)
+    except IOError as e:
+        print(e+"created time")
+
+def getModifiedTime(file_name: str):
+    try:
+        return os.getctime(file_name)
+    except IOError as e:
+        print(e+"modified time")
+
+# Methos
+def saveFileInfoInTXT(node_file_name: str, path: str):
+    DEFAULT_MGS= 'DefaultSentence'
+    try:
+        with open(f'{node_file_name}', 'W') as f:
+            stat_result= str(os.stat(path))
+            f.write(stat_result)
+    except IOError as e:
+        print(e+"stat_result, so set default")
+    finally:
+        with open(f'{node_file_name}', 'W') as f:
+            f.write(DEFAULT_MGS)
+
+def fileNameing(name: str, date: str, created: str, modified: str):
+    TIME= f'{date}){created}_{modified}'
+    # original->new file title (name, str(TIME))
+
+def moveTempsInFolder():
+    # system command "move ."
+
+# Main
+PATH= # get relevent Directory
+DATE= date.today.strftime("%d%m%Y")
+CURRENT_INFOMATION_SUMMTION= f'SUM{DATE}.txt'
+
+file_names= data_createds= date_modifieds= list()
+file_name= data_created= date_modified= str(" ")
+
+file_names= getFileNames()
+saveFileInfoInTXT(CURRENT_INFOMATION_SUMMTION, PATH)
+
+try:
+    for file_name in file_names:
+        data_createds.insert(getCreatedTime(file_name))
+        date_modifieds.insert(getModifiedTime(file_name))
+except IOError as e:
+    print(e+"time list")
+
+# 2. Folder: Current Date
+#    File  : "Created Time" + "-" + "Modified Time"
+m_array_index= 0
+m_array_index= getIndex(m_array_index, file_names)
+for index in range(0, m_array_index+1):
+    fileNameing(file_names[index], data_createds[index], date_modifieds[index])
+file_name= fileNameing(names)
+
+makeFolders(data_createds[index], date_modifieds[index])
+# moveTempsInFolder()
+
+# End
