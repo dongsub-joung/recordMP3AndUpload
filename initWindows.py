38bbad66ab0865657dbbe52e1e33b82d6681c581
dongsub-joung <joungdongsub1103@gmail.com>
2021-10-08 21:25:42 +0900

    Add getFuid()

--- initWindows.py
+++ initWindows.py
@@ -1,3 +1,6 @@
+# detali : Automate workflow on Sound Files. 
+# @auther: Joung DongSub
+# @Date  : 2021-10-08 / 14:57
 # Even when i made about "https://github.com/dongsub-joung/Sociology_bookList",
 # always always i'm alone, had have not a group in or main.
 # Korean is always be korean, so "korea" means the same "the lowest trust society and community commpletly".
@@ -42,21 +45,27 @@ def getModifiedTime(file_name: str):
 
 # Methos
 def saveFileInfoInTXT(node_file_name: str, path: str):
-    DEFAULT_MGS= 'DefaultSentence'
     try:
         with open(f'{node_file_name}', 'w') as f:
             stat_result= str(os.stat(path))
             f.write(stat_result)
     except IOError as e:
         print(e+"stat_result, so set default")
-    finally:
-        with open(f'{node_file_name}', 'w') as f:
-            f.write(DEFAULT_MGS)
 
 def fileNameing(name: str, date: str, created: str, modified: str):
     TIME= f'{date}){created}_{modified}'
     os.rename('Sound recordings', TIME)
-  
+
+def getFuid():
+    # os.system.__code__("curl 'https://up.ufile.io/v1/upload/finalise' \")
+    try:
+        return input()
+    except Exception as e:
+        print(e+"fuid")
+        return str(e)
+
+def uploadingDir(file_name: str, file_type: str, total_chunk: str):
+    return str(respon_oject)
 
 # Main
 DATE= datetime.date.today().strftime("%d%m%Y")
@@ -82,4 +91,9 @@ except IOError as e:
 m_array_index= getIndex(m_array_index, file_names)
 for index in range(0, m_array_index):
     fileNameing(file_names[index], data_createds[index], date_modifieds[index])
+
+
+# 3. Uploading
+fuid: str= getFuid()
+uploadingDir(PATH)
 # End
